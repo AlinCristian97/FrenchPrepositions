671f0402d099e328791c23e8b86d36fa63b4d898
Alin <alin.cristian977@gmail.com>
2026-01-29 18:20:08 +0100

    Create Constants.cs

---
+++ FrenchPrepositions/Constants.cs
@@ -0,0 +1,77 @@
+﻿namespace FrenchPrepositions;
+
+public class Constants
+{
+    public static class PrepositionsCategories
+    {
+        public const string Time = "Time";
+        public const string Place = "Place";
+        public const string Direction = "Direction";
+        public const string Cause = "Cause";
+        public const string Purpose = "Purpose";
+        public const string Means = "Means";
+        public const string Comparison = "Comparison";
+    }
+
+    public static class Time
+    {
+        public const string Avant = "avant";
+        public const string Apres = "après";
+        public const string Pendant = "pendant";
+        public const string Depuis = "depuis";
+        public const string JusquA = "jusqu’à";
+        public const string Des = "dès";
+    }
+
+    public static class Place
+    {
+        public const string A = "à";
+        public const string Dans = "dans";
+        public const string Sur = "sur";
+        public const string Sous = "sous";
+        public const string Entre = "entre";
+        public const string Devant = "devant";
+        public const string Derriere = "derrière";
+        public const string Chez = "chez";
+        public const string En = "en";
+        public const string Vers = "vers";
+        public const string AuPresDe = "auprès de";
+    }
+
+    public static class Direction
+    {
+        public const string Vers = "vers";
+        public const string JusquA = "jusqu’à";
+        public const string De = "de";
+        public const string A = "à";
+        public const string Par = "par";
+    }
+
+    public static class Cause
+    {
+        public const string ACauseDe = "à cause de";
+        public const string GraceA = "grâce à";
+        public const string ParSuiteDe = "par suite de";
+    }
+
+    public static class Purpose
+    {
+        public const string Pour = "pour";
+        public const string AfinDe = "afin de";
+    }
+
+    public static class Means
+    {
+        public const string Avec = "avec";
+        public const string Sans = "sans";
+        public const string Selon = "selon";
+        public const string Par = "par";
+    }
+
+    public static class Comparison
+    {
+        public const string Contre = "contre";
+        public const string Envers = "envers";
+        public const string Selon = "selon";
+    }
+}
