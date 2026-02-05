456b9a92df949993e82eb6bc63bdfb19c16ca354
Alin <alin.cristian977@gmail.com>
2026-02-05 02:55:42 +0100

    Update Constants.cs

--- FrenchPrepositions/Constants.cs
+++ FrenchPrepositions/Constants.cs
@@ -37,6 +37,7 @@ public class Constants
         public const string Vers = "vers";
         public const string AuPresDe = "auprès de";
         public const string Parmi = "parmi";
+        public const string AutourDe = "autour de";
     }
 
     public static class Direction
